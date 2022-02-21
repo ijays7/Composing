8157478bf6e0f48a7b723ed007b51c1b6c1b7c5c
songjie <ijaysdev@gmail.com>
2022-02-21 10:36:08 +0800

    1. Bump up dependencies 2. Upgrade targetSdk to 31, which is Android12

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -5,16 +5,21 @@ object ApplicationId {
     const val id = "com.ijays.composeit"
 }
 
-object Versions {
-    const val kotlin = "1.5.30"
-    const val jetpackCompose = "1.0.3"
+object Config {
+    const val compileSDK = 31
+    const val targetSDk = 31
+    const val minSDK = 21
+}
 
+object Versions {
+    const val kotlin = "1.5.31"
+    const val jetpackCompose = "1.0.5"
 }
 
 object Androidx {
-    const val coreKtx = "androidx.core:core-ktx:1.3.2"
+    const val coreKtx = "androidx.core:core-ktx:1.6.0"
     const val appcompat = "androidx.appcompat:appcompat:1.3.1"
-    const val material = "com.google.android.material:material:1.2.1"
+    const val material = "com.google.android.material:material:1.4.0"
     const val lifecycleKtx = "androidx.lifecycle:lifecycle-runtime-ktx:2.3.1"
     const val livedataKtx = "androidx.lifecycle:lifecycle-livedata-ktx:2.3.1"
     const val activityKtx = "androidx.activity:activity-ktx:1.3.1"
@@ -31,6 +36,7 @@ object Androidx {
 }
 
 object Libraries {
+    const val agp = "com.android.tools.build:gradle:7.0.3"
     const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
 
